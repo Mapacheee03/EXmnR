665994e70063fce336e0f0fedecc49c6696d3373
Mapacheee03 <137364269+Mapacheee03@users.noreply.github.com>
2023-10-06 11:03:50 -0700

    recuperacion1

---
+++ R-ESTRUCTURA/R-ESTRUCTURA/EjerciciosR.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_ESTRUCTURA
+{
+    internal class EjerciciosR
+    {
+       public void CalcularGeneroYFechaNacimiento()
+        {
+            Console.WriteLine("ingresa su curp:");
+            string curp = Console.ReadLine().ToUpper();
+
+            if (curp.Length == 18)
+            {
+                string genero = curp.Substring(10, 1);
+                string fechaNacimiento = curp.Substring(4, 6);
+                string generoCompleto = (genero == "H") ? "Masculino" : "Femenino";
+
+                int anio = int.Parse(fechaNacimiento.Substring(0, 2));
+                int mes = int.Parse(fechaNacimiento.Substring(2, 2));
+                int dia = int.Parse(fechaNacimiento.Substring(4, 2));
+
+                Console.WriteLine("Usted Es De Genero: " + generoCompleto);
+                Console.WriteLine(" Y Su Fecha de Nacimiento: " + dia + "/" + mes + "/" + (anio >= 40 ? "19" : "200") + anio);
+            }
+            else
+            {
+                Console.WriteLine("La CURP ingresada no es válida.");
+            }
+        }
+        
+        public void Intervalos()
+        {
+
+        }
+        
+    }
+    
+}
---
+++ R-ESTRUCTURA/R-ESTRUCTURA/Program.cs
@@ -0,0 +1,9 @@
+﻿using R_ESTRUCTURA;
+
+EjerciciosR ejerciciosR= new EjerciciosR();
+Console.WriteLine("Programa para Identificar sexo y fecha de nacimiento:");
+ejerciciosR.CalcularGeneroYFechaNacimiento();
+
+Console.WriteLine("Programa para ver los Intervalos dos numeros:");
+ejerciciosR.Intervalos();
+
